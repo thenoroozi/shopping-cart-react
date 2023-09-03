2a9270dff82096ec3fda8a755cbfeefa7119dbcb
Hamed Noroozi <noruzi206@gmail.com>
2023-09-03 08:49:04 +0330

    create context and add to app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// Context
+import ProductContextProvider from './context/ProductContextProvider';
+const App = () => {
+   return (
+     <ProductContextProvider>
+      
+     </ProductContextProvider>
+   );
+};
+
+export default App;
---
+++ src/context/ProductContextProvider.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+
+//API
+import { getProducts } from '../services/api';
+
+const ProductsContext=React.createContext();
+const ProductContextProvider = (props) => {
+
+   const [products,setProducts]=useState([]);
+
+   useEffect(()=>{
+      const fetchAPI=async ()=>{
+        setProducts(await getProducts())
+      }
+  
+      fetchAPI();
+   },[])
+
+
+   return (
+      <ProductsContext.Provider value={products}>
+         {props.children}
+      </ProductsContext.Provider>
+   );
+};
+
+export default ProductContextProvider;
