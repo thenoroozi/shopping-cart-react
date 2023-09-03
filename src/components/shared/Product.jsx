3b55e9d3a29e3430b07ee7e7054a790210e06be7
Hamed Noroozi <noruzi206@gmail.com>
2023-09-03 09:53:40 +0330

    move helper to src folder

--- src/components/shared/Product.jsx
+++ src/components/shared/Product.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 //Functions
-import {shorten} from '../helper/functions'
+import {shorten} from '../../helper/functions'
 
 const Product = ({productData}) => {
    return (
