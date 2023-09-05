61067886a9b24d5886a676d09a69d3edc89b2972
Hamed Noroozi <noruzi206@gmail.com>
2023-09-05 10:42:40 +0330

    update shop cart component

--- src/components/ShopCart.jsx
+++ src/components/ShopCart.jsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useCallback, useContext } from 'react';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+//components
+import Cart from './shared/Cart';
+
+//Context
+import { CartContext } from '../context/CartContextProvider';
 
 const ShopCart = () => {
+
+   const { state, dispatch } = useContext(CartContext);
+
    return (
       <div>
-         shop cart
+         <div>
+            {state.selectedItems.map(item => <Cart key={item.id} data={item} />)}
+         </div>
+         {
+            state.itemsCounter > 0 && <div>
+               <p><span>Total Itam:</span> {state.itemsCounter}</p>
+               <p><span>Total Payments:</span> {state.total}</p>
+               <div>
+                  <button onClick={()=> dispatch({type: "CHECKOUT"})}>Check Out</button>
+                  <button onClick={()=> dispatch({type: "CLEAR"})}>Clear</button>
+               </div>
+            </div>
+         }
+         {
+            state.checkout &&
+            <div>
+               <h3>Check Out Successfully</h3>
+               <Link to='/products'>Buy More</Link>
+            </div>
+         }
+         {
+            !state.checkout && state.itemsCounter === 0 &&
+            <div>
+               <h3>Want to Buy?</h3>
+               <Link to='/products'>Go Back to Shop</Link>
+            </div>
+         }
       </div>
    );
 };
