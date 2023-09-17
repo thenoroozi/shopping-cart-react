ffd5fdd1ae353554526fa5ac08833e2a85d1ecd2
Hamed Noroozi <noruzi206@gmail.com>
2023-09-17 15:30:09 +0330

    add styles of product Details

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,28 +1,32 @@
 import React, { useContext } from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 //Context
 import { ProductsContext } from '../context/ProductContextProvider';
+//styles
+import styles from './ProductDetails.module.css';
 
 const ProductDetails = (props) => {
 
-   const id=props.match.params.id;
+   const id = props.match.params.id;
    const data = useContext(ProductsContext);
-   const product= data[id -1]; 
-   const {image,title,description, price , category}=product;
+   const product = data[id - 1];
    
+   const { image, title, description, price, category } = product;
+  
+
    return (
-      <div>
-        <img src={image} alt="product image" />
-        <div>
-         <h3>{title}</h3>
-         <p>{description}</p>
-         <p><span>Category: </span>{category}</p>
-         <div>
-            <span>{price} $</span>
-            <Link to='/products'>Back to shop</Link>
+      <div className={styles.container}>
+         <img className={styles.image} src={image} alt="product image" />
+         <div className={styles.textContainer}>
+            <h3>{title}</h3>
+            <p className={styles.description}>{description}</p>
+            <p className={styles.category}><span>Category: </span>{category}</p>
+            <div className={styles.buttonContainer}>
+               <span className={styles.price}>{price} $</span>
+               <Link to='/products'>Back to shop</Link>
+            </div>
          </div>
-        </div>
       </div>
    );
 };
