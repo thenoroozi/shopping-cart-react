bffd40bb804d3c13affba841fb862be77757573b
Hamed Noroozi <noruzi206@gmail.com>
2023-09-17 15:31:08 +0330

    add styles of shopCart

--- src/components/ShopCart.jsx
+++ src/components/ShopCart.jsx
@@ -5,38 +5,40 @@ import Cart from './shared/Cart';
 
 //Context
 import { CartContext } from '../context/CartContextProvider';
-
+//Styles
+import styles from './shopCart.module.css'
 const ShopCart = () => {
 
    const { state, dispatch } = useContext(CartContext);
 
    return (
-      <div>
-         <div>
+      <div className={styles.container}>
+         <div className={styles.cartContainer}>
             {state.selectedItems.map(item => <Cart key={item.id} data={item} />)}
          </div>
          {
-            state.itemsCounter > 0 && <div>
+            state.itemsCounter > 0 && <div className={styles.payments}>
                <p><span>Total Itam:</span> {state.itemsCounter}</p>
                <p><span>Total Payments:</span> {state.total}</p>
-               <div>
-                  <button onClick={()=> dispatch({type: "CHECKOUT"})}>Check Out</button>
-                  <button onClick={()=> dispatch({type: "CLEAR"})}>Clear</button>
+               <div className={styles.buttonContainer}>
+                  <button className={styles.clear} onClick={() => dispatch({ type: "CHECKOUT" })}>Check Out</button>
+                  <button className={styles.checkout} onClick={() => dispatch({ type: "CLEAR" })}>Clear</button>
                </div>
             </div>
          }
          {
-            state.checkout &&
-            <div>
-               <h3>Check Out Successfully</h3>
-               <Link to='/products'>Buy More</Link>
-            </div>
-         }
-         {
             !state.checkout && state.itemsCounter === 0 &&
-            <div>
+            <div className={styles.complete}>
                <h3>Want to Buy?</h3>
                <Link to='/products'>Go Back to Shop</Link>
+            </div>
+         }
+
+         {
+            state.checkout &&
+            <div className={styles.complete}>
+               <h3>Check Out Successfully</h3>
+               <Link to='/products'>Buy More</Link>
             </div>
          }
       </div>
