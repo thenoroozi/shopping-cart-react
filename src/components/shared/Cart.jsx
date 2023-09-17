69896666ba4c0c31b437d7af4a8856087282c484
Hamed Noroozi <noruzi206@gmail.com>
2023-09-17 15:27:17 +0330

    add styles of Cart

--- src/components/shared/Cart.jsx
+++ src/components/shared/Cart.jsx
@@ -8,6 +8,8 @@ import { shorten } from '../../helper/functions';
 
 //Icons
 import trashIcon from "../../assets/icons/trash.svg";
+//styles
+import styles from './Cart.module.css';
 
 const Cart = (props) => {
 
@@ -15,16 +17,16 @@ const Cart = (props) => {
    const { image, title, price, quantity } = props.data;
 
    return (
-      <div>
-         <img src={image} alt="product image" />
-         <div>
+      <div className={styles.container}>
+         <img className={styles.productImage} src={image} alt="product image" />
+         <div className={styles.data}>
             <h3>{shorten(title)}</h3>
             <p>{price}</p>
          </div>
          <div>
-            <span>{quantity}</span>
+            <span className={styles.quantity}>{quantity}</span>
          </div>
-         <div>
+         <div className={styles.buttonContainer}>
             {
                quantity > 1 ?
                <button onClick={()=> dispatch({type:"DECREASE",payload: props.data})}>-</button>:
